085d73013d4b5a0ef43e1e7823cd7d8c0b302bdf
a-s-t-e-y-a <krishnoit100@gmail.com>
2025-09-25 05:56:49 +0000

    UPADTED LANGUAGE SUPPORT AND JSON SUPPORT EXPLICITLY IN C++ C AND JAVA

--- packages/node/20.11.1/build.sh
+++ packages/node/20.11.1/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl "https://nodejs.org/dist/v20.11.1/node-v20.11.1-linux-x64.tar.xz" -o node.tar.xz
+curl "https://nodejs.org/dist/v24.0.0/node-v24.0.0-linux-x64.tar.xz" -o node.tar.xz
 tar xf node.tar.xz --strip-components=1
 rm node.tar.xz
